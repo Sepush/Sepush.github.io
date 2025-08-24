8bd5c591105072d4d24a190ef63a327983d40e5a
Artea <sepush@outlook.com>
2025-08-24 16:31:58 +0800

    fix: type

--- shims.d.ts
+++ shims.d.ts
@@ -1,27 +1,11 @@
-import type { AttributifyAttributes } from '@unocss/preset-attributify'
-
-declare module '@vue/runtime-dom' {
-  interface HTMLAttributes extends AttributifyAttributes { }
-}
-declare module '@vue/runtime-dom' {
-  interface HTMLAttributes {
-    [key: string]: any
-  }
-}
-declare module '@vue/runtime-core' {
-  interface AllowedComponentProps {
-    [key: string]: any
-  }
-}
-
-declare module 'react' {
-  interface HTMLAttributes<T> extends AttributifyAttributes { }
-}
+/// <reference types="unocss/preset-attributify" />
 
 declare global {
   namespace astroHTML.JSX {
-    interface HTMLAttributes extends AttributifyAttributes { }
+    interface HTMLAttributes {
+      [key: string]: any
+    }
   }
 }
 
-export { }
+export {}
