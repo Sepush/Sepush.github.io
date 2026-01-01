63ae1c448728212a5946a104c79078faf8078132
Artea <sepush@outlook.com>
2026-01-02 02:23:58 +0800

    Add some friends

--- src/config/social.ts
+++ src/config/social.ts
@@ -67,4 +67,77 @@ export const friends: Friend[] = [
       },
     ],
   },
+  {
+    name: "SoonIter",
+    description: "Hello world! Today I will make you even better.",
+    socialLinks: [
+      {
+        platform: "website",
+        url: "https://sooniter.site/",
+        icon: "i-tabler-link",
+      },
+      {
+        platform: "github",
+        url: "https://github.com/sooniter",
+        icon: "i-tabler-brand-github",
+      },
+      {
+        platform: "x",
+        url: "https://twitter.com/Soon_Iter",
+        icon: "i-tabler-brand-x",
+      },
+    ],
+  },
+  {
+    name: "SkyWT",
+    description: "Passionate software designer & engineer. Big Tech employee by day, freelance developer by night.",
+    socialLinks: [
+      {
+        platform: "website",
+        url: "https://skywt.net/",
+        icon: "i-tabler-link",
+      },
+      {
+        platform: "github",
+        url: "https://github.com/Skywt2003",
+        icon: "i-tabler-brand-github",
+      },
+      {
+        platform: "x",
+        url: "https://x.com/skywt2003",
+        icon: "i-tabler-brand-x",
+      },
+      {
+        platform: "telegram",
+        url: "https://t.me/skywt2003",
+        icon: "i-tabler-brand-telegram",
+      },
+      {
+        platform: "email",
+        url: "mailto:me@skywt.cn",
+        icon: "i-tabler-mail",
+      },
+    ],
+  },
+  {
+    name: "Cyan",
+    description: "A random guy on the internet, and a software engineer.",
+    socialLinks: [
+      {
+        platform: "website",
+        url: "https://cyandev.app/",
+        icon: "i-tabler-link",
+      },
+      {
+        platform: "github",
+        url: "https://github.com/unixzii",
+        icon: "i-tabler-brand-github",
+      },
+      {
+        platform: "x",
+        url: "https://twitter.com/unixzii",
+        icon: "i-tabler-brand-x",
+      },
+    ],
+  },
 ];
